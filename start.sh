fc5675e9ff17661eeb3b01b83f8aef9b172303e8
Snhvn <snhvnnguyen@gmail.com>
2025-08-18 10:59:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
-
+echo "Muốn bao nhiêu disk (ví dụ 128g thì ghi 128)"
+read disk1
+echo "Bạn muốn bao nhiêu ram (ví dụ 3gb thì ghi 3)"
+read ram1
 DISK="/data/vm.qcow2"
 IMG="/opt/qemu/ubuntu.img"
 SEED="/opt/qemu/seed.iso"
-
+DISK1="$disk1"
+RAM="$ram1"
 echo "Creating VM disk..."
 qemu-img convert -f qcow2 -O qcow2 "$IMG" "$DISK"
-qemu-img resize "$DISK" 30G
+qemu-img resize "$DISK" "$DISK1"G
 # Start VM
 qemu-system-x86_64 \
     -m "$RAM"G \
