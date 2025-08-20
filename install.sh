8eba23c3d895bef182ae7555262ee91f1dbd461a
MoonlightPanel <haminhchiennoi@gmail.com>
2025-08-20 14:38:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Code By SNIPA VN"
-# Tải package bổ sung vào để tải:)))
+echo "ReCode by NoiMC"
 apt-get update && apt-get install -y --no-install-recommends \
     qemu-system-x86 \
     qemu-utils \
@@ -17,21 +16,14 @@ apt-get update && apt-get install -y --no-install-recommends \
     net-tools \
     netcat-openbsd \
     && rm -rf /var/lib/apt/lists/* && apt clean
-
-# Tạo thư mục bên trong noVNC
+    
 mkdir -p /data /novnc /opt/qemu /cloud-init
-
-# Tải ubuntu 22.04 image
 curl -L https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img \
 -o /opt/qemu/ubuntu.img
-
-# Viết host
-echo "instance-id: servertipacvn\nlocal-hostname: servertipacvn" > /cloud-init/meta-data
-
-# Chỉnh sửa config image ubuntu
+echo "instance-id: idlernetwork\nlocal-hostname: idlernetwork" > /cloud-init/meta-data
 printf "#cloud-config\n\
 preserve_hostname: false\n\
-hostname: servertipacvn\n\
+hostname: idlernetwork\n\
 users:\n\
   - name: root\n\
     gecos: root\n\
@@ -48,21 +40,9 @@ chpasswd:\n\
 runcmd:\n\
   - systemctl enable ssh\n\
   - systemctl restart ssh\n" > /cloud-init/user-data
-
-# Tạo Cloud-Iso cho image ubuntu 22.04
 genisoimage -output /opt/qemu/seed.iso -volid cidata -joliet -rock \
 /cloud-init/user-data /cloud-init/meta-data
-
-# Setup noVNC
-curl -L https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.zip -o /tmp/novnc.zip && \
-unzip /tmp/novnc.zip -d /tmp && \
-mv /tmp/noVNC-1.3.0/* /novnc && \
-rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
-# Cài đặt cloudflare
-sudo mkdir -p --mode=0755 /usr/share/keyrings && \
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null && echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list && sudo apt-get update && sudo apt-get install cloudflared
-# Tải file start.sh để bắt đầu khởi chạy
-wget -O start.sh https://github.com/Snhvn/Ubuntu123/raw/refs/heads/main/start1.sh
+wget -O start.sh https://github.com/MoonlightPanel/Ubuntu123/raw/refs/heads/main/start.sh
 clear
 echo "Đang chạy vui lòng đừng tắt"
 chmod +x start.sh && \
