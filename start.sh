c622b720df6c2f8fd1abd0cb5f0f2d3a8f415d49
MoonlightPanel <haminhchiennoi@gmail.com>
2025-08-20 14:35:55 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,30 +15,11 @@ qemu-img resize "$DISK" "$DISK1"G
 # Start VM
 qemu-system-x86_64 \
     -m "$RAM"G \
+    -cpu max \
+    -accel tcg,thread=multi \
     -drive file="$DISK",format=qcow2,if=virtio \
     -drive file="$SEED",format=raw,if=virtio \
     -netdev user,id=net0,hostfwd=tcp::2222-:22 \
     -device virtio-net,netdev=net0 \
     -vga virtio \
-    -display vnc=:0 \
-    -daemonize
-
-# Start noVNC
-websockify --web=/novnc 6080 localhost:5900 &
-
-echo "================================================"
-echo " 🖥️  VNC: http://localhost:6080/vnc.html"
-echo " 🔐 SSH: ssh root@localhost -p 2222"
-echo " 🧾 Login: root / root"
-echo " Supported Code Sandbox (use ngrok or cloudflare)"
-echo " Code By Snipavn/Snhvn (Github) Youtube: https://youtube.com/@snipavn205 & Youtube: HopingBoyz" 
-echo "================================================"
-
-# Wait for SSH port to be ready
-for i in {1..30}; do
-  nc -z localhost 2222 && echo "✅ VM is ready!" && break
-  echo "⏳ Waiting for SSH..."
-  sleep 2
-done
-
-wait
+    -nographic
